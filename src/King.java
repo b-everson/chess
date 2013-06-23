a35689e20af35b2136fdf682a0d347a704b092b6
b-everson <b_everson@yahoo.com>
2013-06-22 17:56:35 -0700

    added kings move functionality

--- src/King.java
+++ src/King.java
@@ -8,11 +8,33 @@ public class King extends ChessPiece{
   
   public ArrayList<BoardPosition> checkMoveAvailable(){
     ArrayList<BoardPosition> possibilities = new ArrayList<BoardPosition>();
-	possibilities.add(new BoardPosition(1,1));
+	int x = position.getX();
+	int y = position.getY();
+	BoardPosition[] positions = new BoardPosition[8];
+	positions[0] = gameBoard.getBoardPosition(x + 1, y);
+	positions[1] = gameBoard.getBoardPosition(x - 1, y);
+	positions[2] = gameBoard.getBoardPosition(x + 1, y + 1);
+	positions[3] = gameBoard.getBoardPosition(x - 1, y + 1);
+	positions[4] = gameBoard.getBoardPosition(x + 1, y - 1);
+	positions[5] = gameBoard.getBoardPosition(x - 1, y - 1);
+	positions[6] = gameBoard.getBoardPosition(x, y + 1);
+	positions[7] = gameBoard.getBoardPosition(x, y - 1);
+	for (int i = 0; i < positions.length; i++){
+	  if(positions[i] != null){
+		if(!positions[i].occupiedByFriendly(this)){
+		//  if(!isVulnerable(positions[i]))   
+		    possibilities.add(positions[i]);
+		}
+	  }
+	}
 	return possibilities;
   }
   
   public boolean move(BoardPosition position){
-    return false;
+    if(isVulnerable(position)){
+	  System.out.println("Cannot move your king into check.");
+	  return false;
+	}  
+	return super.move(position);
   }
 }
