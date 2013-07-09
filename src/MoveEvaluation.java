621d4fd3ee81ef917da18719fafab3eab364c823
b-everson <b_everson@yahoo.com>
2013-07-09 11:53:33 -0700

    Added MoveEvaluation constructor that takes Move,Player as parameters

--- src/MoveEvaluation.java
+++ src/MoveEvaluation.java
@@ -2,6 +2,11 @@ public class MoveEvaluation{
   private Move myMove;
   private int myScore;
   
+  public MoveEvaluation(Move move, Player player){
+    myMove = move;
+	myScore = player.evaluateBoard();
+  }
+  
   public MoveEvaluation(Move move, int score){
     myMove = move;
 	myScore = score;
